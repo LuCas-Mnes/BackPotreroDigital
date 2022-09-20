8f852efde4b1ecac3270c07f60d06cea375bc601
Lucas Manuel Ruiz <69923008+LuCas-Mnes@users.noreply.github.com>
2022-09-19 22:02:34 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tp 1 BackEnd</title>
+</head>
+
+<body>
+
+    <!--Imprima por pantalla: “Hola mundo”. -->
+    <?php
+
+    $Saludo = "“Hola Mundo";
+    print "Hola mundo";
+    ?>
+
+</body>
+
+</html>
